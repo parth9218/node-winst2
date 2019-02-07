0d80759d614d95ca63e7d2bfe14382fea083c661
parth <prajapatiparth06@gmail.com>
2019-02-07 17:40:50 +0530

    First

---
+++ myApp/config/winston.js
@@ -0,0 +1,37 @@
+let appRoot = require('app-root-path');
+let winston = require('winston');
+
+var options = {
+    file:   {
+        level: 'info',
+        filename: `${appRoot}/logs/app.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5342880,
+        maxFiles: 5,
+        colorize: false
+    },
+    console:    {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+    }
+};
+
+
+var logger = new winston.createLogger({
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false
+});
+
+logger.stream = {
+    write: (message, encoding) => {
+        logger.info(message);
+    }
+}
+
+module.exports = logger;
